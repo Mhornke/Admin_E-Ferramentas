4c486818d84ca3553947cb4eb95735320e957967
MATHEUS DUARTE HORNKE <163477232+Mhornke@users.noreply.github.com>
2024-12-12 14:59:57 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  images: {
-    domains: ['unsplash.com', 'istockphoto.com', 'freepik.com'], // Adicionando o domínio do Freepik
+    domains: ['unsplash.com', 'media.istockphoto.com', 'www.istockphoto.com', 'freepik.com'], // Adicionando o domínio do Freepik
   },
 };
 
